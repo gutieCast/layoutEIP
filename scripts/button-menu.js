64cf1f8408e9938f2fbf1797a18eb354390fb191
gutieCast <agutiecast@gmail.com>
2022-10-12 08:25:06 -0400

    presentable

--- scripts/button-menu.js
+++ scripts/button-menu.js
@@ -1,19 +1,26 @@
 const body = document.querySelector('body');
-        const nav = document.querySelector('.nav-menu');
-        const btn = document.querySelector('#menu-button-toggle');
-    
-    
-    const navMobileToggle = () => {
-        body.classList.toggle('nav-open');
-        addMenu();
-    }
-    
-    btn.addEventListener('click', event => {
-        event.preventDefault;
-        navMobileToggle();
-    });
+const nav = document.querySelector('.nav-menu');
+const btn = document.querySelector('#menu-button-toggle');
+const linksMenu = [...document.getElementsByClassName('link-menu')];
+   
+const navMobileToggle = () => {
+    body.classList.toggle('nav-open');
+    // addMenu();
+}
 
-    
-    const addMenu = () => {
-        nav.appendChild(ul);
-    }
+btn.addEventListener('click', event => {
+    event.preventDefault;
+    navMobileToggle();
+});
+
+const addMenu = () => {
+    nav.appendChild(ul);
+}
+
+const closeNavMobile = () => {
+    body.classList.remove('nav-open')
+}
+
+linksMenu.forEach(link => {
+    link.addEventListener('click', closeNavMobile);
+});
--- scripts/typewriter.js
+++ scripts/typewriter.js
@@ -1,6 +1,6 @@
 const parallax = document.getElementsByClassName('section'[0]);
 let spaceToType = document.querySelector("#type-animation")
-const toType = ["escribo poesía. \ <br>\ escribo."];
+const toType = ["escribo poesía."];
 let textPosition = 0;
 let speed = 100;
 
@@ -14,14 +14,11 @@ const typewriter = async () => {
     if (textPosition++ != toType[0].length) {
       setTimeout(typewriter, speed);
     } else {
-      await delay(2500);
+      await delay(1700);
       textPosition = 0;
       typewriter()
     }
 }
 
-console.log(parallax);
-  
-
-      window.addEventListener("load", typewriter)
+window.addEventListener("load", typewriter)
 
